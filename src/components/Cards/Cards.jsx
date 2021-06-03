1e43d383998d1b7d9b87def9a67838791232492a
ajo01 <amyjojjm2001@gmail.com>
2021-06-03 02:16:03 -0700

    countup with real data

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { Card, CardContent, Typography, Grid } from '@material-ui/core'
+import CountUp from 'react-countup'
 
 import styles from './Cards.module.css'
 
-const Cards = (props) => {
-    console.log(props.data)
+const Cards = ({data: {confirmed, recovered, deaths, lastUpdate}}) => {
+    if (!confirmed) {
+        return 'Loading...'
+    }
 
     return (
         <div className={styles.container}>
@@ -12,8 +15,15 @@ const Cards = (props) => {
                 <Grid item component={Card}>
                     <CardContent>
                         <Typography color="textSecondary" gutterBottom>Infected</Typography>
-                        <Typography variant="h5">Real Data</Typography>
-                        <Typography color="textSecondary" gutterBottom>Real Date</Typography>
+                        <Typography variant="h5">
+                            <CountUp 
+                            start={0}
+                            end={confirmed.value}
+                            duration={4}
+                            separator=","
+                            />
+                        </Typography>
+                        <Typography color="textSecondary" gutterBottom>{new Date(lastUpdate).toDateString()}</Typography>
                         <Typography variant="body2">Active Number of Cases</Typography>
                     </CardContent>
                 </Grid>
@@ -21,7 +31,14 @@ const Cards = (props) => {
                 <Grid item component={Card}>
                     <CardContent>
                         <Typography color="textSecondary" gutterBottom>Recovered</Typography>
-                        <Typography variant="h5">Real Data</Typography>
+                        <Typography variant="h5">
+                        <CountUp 
+                            start={0}
+                            end={recovered.value}
+                            duration={3}
+                            separator=","
+                            />
+                        </Typography>
                         <Typography color="textSecondary" gutterBottom>Real Date</Typography>
                         <Typography variant="body2">Number of Recoviers</Typography>
                     </CardContent>
@@ -30,7 +47,14 @@ const Cards = (props) => {
                 <Grid item component={Card}>
                     <CardContent>
                         <Typography color="textSecondary" gutterBottom>Deaths</Typography>
-                        <Typography variant="h5">Real Data</Typography>
+                        <Typography variant="h5">
+                        <CountUp 
+                            start={0}
+                            end={deaths.value}
+                            duration={2}
+                            separator=","
+                            />
+                        </Typography>
                         <Typography color="textSecondary" gutterBottom>Real Date</Typography>
                         <Typography variant="body2">Number of Deaths from covid</Typography>
                     </CardContent>
