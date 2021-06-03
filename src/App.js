7561e940137f0af1692ef1f8dd56710d06b1706c
ajo01 <amyjojjm2001@gmail.com>
2021-06-03 01:23:52 -0700

    modified data from api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
 
 import {Cards, Chart, CountryPicker} from './components'
 import styles from './App.module.css'
+import {fetchData} from './api'
 
 class App extends React.Component {
+    async componentDidMount() {
+        const data = await fetchData()
+        console.log(data)
+    }
+
     render() {
         return (
-            <div>
+            <div className={styles.container}>
                 <Cards/>
                 <Chart/>
                 <CountryPicker/>
