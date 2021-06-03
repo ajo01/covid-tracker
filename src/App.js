1dc1a5ac8bf376f3811408c11ac5725add1900c3
ajo01 <amyjojjm2001@gmail.com>
2021-06-03 14:31:56 -0700

    api fetch by country working

--- src/App.js
+++ src/App.js
@@ -13,10 +13,15 @@ class App extends React.Component {
     async componentDidMount() {
         const fetchedData = await fetchData()
 
+        console.log(fetchedData)
+
         this.setState({data:fetchedData})
     }
 
     handleCountryChange = async (country) => {
+        const fetchedData = await fetchData(country)
+
+        console.log(fetchedData)
         console.log(country)
     }
 
