a44a4c6b9e807dd563308b17337d345c6102e0d6
ajo01 <amyjojjm2001@gmail.com>
2021-06-03 11:49:16 -0700

    line chart displays

--- src/api/index.js
+++ src/api/index.js
@@ -14,6 +14,23 @@ export const fetchData = async () => {
         return modifiedData
 
     } catch(e) {
+        console.log(e)
+    }
+}
 
+export const fetchDailyData = async () => {
+    try {
+        const {data} = await axios.get(`${url}/daily`)
+
+        const modifiedData = data.map((dailyData) => ({
+            confirmed: dailyData.confirmed.total,
+            deaths: dailyData.deaths.total,
+            date: dailyData.reportDate  
+        }))
+
+        return modifiedData
+
+    } catch(e) {
+        console.log(e)
     }
 }
