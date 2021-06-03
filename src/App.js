77403c25fe32d96152afdd0448baa5f583ab7a53
ajo01 <amyjojjm2001@gmail.com>
2021-06-03 14:48:58 -0700

    barchart works

--- src/App.js
+++ src/App.js
@@ -13,8 +13,6 @@ class App extends React.Component {
     async componentDidMount() {
         const fetchedData = await fetchData()
 
-        console.log(fetchedData)
-
         this.setState({data:fetchedData})
     }
 
--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -43,12 +43,12 @@ const Chart = ({data: {confirmed, recovered, deaths}, country}) => {
                 labels: ['Infected', 'Recovered', 'Deaths'],
                 datasets: [{
                     label: 'People',
-                    backGround: [
+                    backgroundColor: [
                         'rgba(0,0,255,0.5)',
                         'rgba(0,255,0,0.5)',
                         'rgba(255,0,0,0.5)'  
                     ],
-                    data:[confirmed, recovered, deaths]
+                    data:[confirmed.value, recovered.value, deaths.value]
                 }]
             }} 
             options={{
